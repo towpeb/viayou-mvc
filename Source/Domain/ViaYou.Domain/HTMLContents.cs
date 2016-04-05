1ad4d27a9010a02705d295a1d5bb412195ae28db
Roly <rolycg89@gmail.com>
2016-04-05 18:32:39 -0400

    HTMLContent model added

---
+++ Source/Domain/ViaYou.Domain/HTMLContents.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ViaYou.Domain.Enums;
+
+namespace ViaYou.Domain
+{
+    public class HTMLContents
+    {
+        public int ID { get; set; }
+        [Required]
+        public string text { get; set; }
+        [Required]
+        public Region region { get; set; }
+        public int order { get; set; }
+    }
+}
---
+++ Source/Domain/ViaYou.Domain/Repositories/IHTMLContentRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViaYou.Domain.Repositories
+{
+    interface IHTMLContentRepository
+    {
+        HTMLContents GetById(int id);
+        void Add(HTMLContents testimonial);
+        IQueryable<HTMLContents> GetAll();
+    }
+}
