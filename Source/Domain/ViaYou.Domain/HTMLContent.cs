18b3fc2596f49e4413bb879413d848e26d2bf8dd
Roly <rolycg89@gmail.com>
2016-04-06 12:48:43 -0400

    Fixing names :), again

---
+++ Source/Domain/ViaYou.Domain/HTMLContent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ViaYou.Domain.Enums;
+
+namespace ViaYou.Domain
+{
+    public class HTMLContent
+    {
+        public int ID { get; set; }
+        [Required]
+        public string Text { get; set; }
+        [Required]
+        public Region Region { get; set; }
+        public int Order { get; set; }
+    }
+}
