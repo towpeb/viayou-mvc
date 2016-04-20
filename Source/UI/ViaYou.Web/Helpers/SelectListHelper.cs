499ca950e9b070bc8e7b3b5389103b11fb7d6562
Roly <rolycg89@gmail.com>
2016-04-20 19:10:51 -0400

    helper for selectList added

---
+++ Source/UI/ViaYou.Web/Helpers/SelectListHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ViaYou.Web.Helpers
+{
+    public static class SelectListHelper
+    {
+        public static IEnumerable<SelectListItem> CreateSelectListItems<T>(this IEnumerable<T> items,
+           Func<T, string> getText, Func<T, string> getValue)
+        {
+            foreach (var item in items)
+                yield return new SelectListItem()
+                {
+                    Text = getText(item),
+                    Value = getValue(item)
+
+                };
+        }
+    }
+}
