6f406411e9211be341c5d85234cd32b31d312719
Roly <rolycg89@gmail.com>
2016-06-11 18:45:03 -0400

    CountryRepo added

---
+++ CountryRepository.cs
@@ -0,0 +1,35 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using ViaYou.Domain;
+using ViaYou.Domain.Repositories;
+
+namespace ViaYou.Data.Repositories
+{
+   public class CountryRepository : BaseRepository,ICountryRepository
+    {
+        public void Add(Country country)
+        {
+            Context.Countries.Add(country);
+        }
+
+        public IQueryable<Country> GetAll()
+        {
+            return Context.Countries;
+        }
+
+       public void Delete(int id)
+       {
+           var country = new Country() {Id = id};
+           Context.Countries.Attach(country);
+           Context.Countries.Remove(country);
+       }
+
+       public Country GetById(int id)
+        {
+            return Context.Countries
+                            .Where(c=>c.Id == id)
+                            .Include(x=>x.Cities)
+                            .FirstOrDefault();
+        }
+    }
+}
